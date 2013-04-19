7b153ff5084c7be2ae3921bd82b9e2cdd8655e7f
Anatol Manoila <anatol.manoila@gmail.com>
2013-04-19 23:29:42 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StSpace::Application.config.secret_key_base = '609d60c8c8c989dee62740adca94225322d2b7dcfc8df40b1b8e8c3cc0c4de239162b4189be122fb4590f69df0c095d5f9c1e49c184bac1d6ae32ab1116a997b'
